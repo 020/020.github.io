0193513ace783176d4701b5a8a3fb0b75928a997
020 <vduong2@gmail.com>
2020-03-20 13:54:45 +0800

    Added jquery for copyright year date

--- main.js
+++ main.js
@@ -387,3 +387,12 @@ function searchType() {
             });
     }
 }
+
+// Body js
+var dteNow = new Date();
+var intYear = dteNow.getFullYear();
+
+$('#copyright').each(function() {
+    var text = $(this).text();
+    $(this).text(text.replace('CurrentCopyrightYear', intYear)); 
+});
